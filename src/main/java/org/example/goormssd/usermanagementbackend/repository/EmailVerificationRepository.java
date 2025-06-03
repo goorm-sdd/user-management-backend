a335c25c046931b9c6fda72b40371c16713a1d7b
projectmiluju <dnjsdyd0712@naver.com>
2025-06-04 07:42:29 +0900

    feat(signup): EmailVerificationRepository 추가

---
+++ src/main/java/org/example/goormssd/usermanagementbackend/repository/EmailVerificationRepository.java
@@ -0,0 +1,10 @@
+package org.example.goormssd.usermanagementbackend.repository;
+
+import org.example.goormssd.usermanagementbackend.domain.EmailVerification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmailVerificationRepository extends JpaRepository<EmailVerification, String> {
+    Optional<EmailVerification> findByCode(String code);
+}
