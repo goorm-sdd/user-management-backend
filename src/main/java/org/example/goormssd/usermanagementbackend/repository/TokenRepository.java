5d5d0373268fcfe3767caef2800f5286ae3d8144
projectmiluju <dnjsdyd0712@naver.com>
2025-06-04 05:35:09 +0900

    fix(token): TokenRepository의 deleteAllByCustomer 메서드 주석 처리

--- src/main/java/org/example/goormssd/usermanagementbackend/repository/TokenRepository.java
+++ src/main/java/org/example/goormssd/usermanagementbackend/repository/TokenRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 public interface TokenRepository extends JpaRepository<Token, Integer> {
     Optional<Token> findByAccessToken(String accessToken);
     Optional<Token> findByRefreshToken(String refreshToken);
-    void deleteAllByCustomer(Member member);
+//    void deleteAllByCustomer(Member member);
 }
