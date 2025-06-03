39db01d4981d934ab51499f9ec93c406e1485caa
projectmiluju <dnjsdyd0712@naver.com>
2025-06-04 07:43:38 +0900

    feat(signup): EmailVerificationRepository

--- src/main/java/org/example/goormssd/usermanagementbackend/repository/EmailVerificationRepository.java
+++ src/main/java/org/example/goormssd/usermanagementbackend/repository/EmailVerificationRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, String> {
+    // 이메일 인증 코드로 찾기 (링크 클릭 시 검증용)
     Optional<EmailVerification> findByCode(String code);
 }
