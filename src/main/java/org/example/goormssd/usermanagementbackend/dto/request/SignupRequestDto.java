5928444483c3c977a61099ca5e9a5f67187d1ee3
projectmiluju <dnjsdyd0712@naver.com>
2025-06-04 04:58:11 +0900

    feat(signup): SignupRequestDto 기본 구조 생성

---
+++ src/main/java/org/example/goormssd/usermanagementbackend/dto/request/SignupRequestDto.java
@@ -0,0 +1,30 @@
+package org.example.goormssd.usermanagementbackend.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupRequestDto {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String passwordCheck;
+
+    @NotBlank
+    private String phoneNumber;
+
+
+}
