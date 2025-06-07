1946bd6be7133fcc088199b3aa8a1e6c2ee4b04d
projectmiluju <dnjsdyd0712@naver.com>
2025-06-08 08:59:44 +0900

    feat(list): AdminMemberController 클래스 추가 및 대시보드 조회 API 구현

---
+++ src/main/java/org/example/goormssd/usermanagementbackend/controller/AdminMemberController.java
@@ -0,0 +1,32 @@
+package org.example.goormssd.usermanagementbackend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.goormssd.usermanagementbackend.dto.response.ApiResponseDto;
+import org.example.goormssd.usermanagementbackend.dto.response.DashboardResponseDto;
+import org.example.goormssd.usermanagementbackend.service.AdminMemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/users")
+@RequiredArgsConstructor
+public class AdminMemberController {
+
+    private final AdminMemberService adminMemberService;
+
+    @GetMapping("/dashboard")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ApiResponseDto<DashboardResponseDto>> getDashboard(
+            @RequestParam(name = "pageNum",   defaultValue = "1")  int pageNum,
+            @RequestParam(name = "pageLimit", defaultValue = "10") int pageLimit
+    ) {
+        DashboardResponseDto responseDto = adminMemberService.getDashboard(pageNum, pageLimit);
+        ApiResponseDto<DashboardResponseDto> response =
+                ApiResponseDto.of(200, "대시 보드 조회 성공", responseDto);
+        return ResponseEntity.ok(response);
+    }
+}
