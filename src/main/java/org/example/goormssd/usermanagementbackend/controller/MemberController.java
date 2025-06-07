583e2fae41497833ebad1f6667df0ba3010179fa
projectmiluju <dnjsdyd0712@naver.com>
2025-06-08 07:13:30 +0900

    feat(profile): 사용자 프로필 정보를 조회하는 getMyProfile 메서드 추가 및 API 경로 수정

--- src/main/java/org/example/goormssd/usermanagementbackend/controller/MemberController.java
+++ src/main/java/org/example/goormssd/usermanagementbackend/controller/MemberController.java
@@ -5,31 +5,30 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.goormssd.usermanagementbackend.dto.request.EmailCheckRequestDto;
 import org.example.goormssd.usermanagementbackend.dto.request.SignupRequestDto;
+import org.example.goormssd.usermanagementbackend.dto.response.ApiResponseDto;
+import org.example.goormssd.usermanagementbackend.dto.response.MyProfileResponseDto;
 import org.example.goormssd.usermanagementbackend.service.MemberService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/signup")
+    @PostMapping("/auth/signup")
     public ResponseEntity<String> signup(@Valid @RequestBody SignupRequestDto requestDto) {
         // 회원 저장 및 이메일 인증 처리까지 서비스에서 수행
         memberService.signup(requestDto);
         return ResponseEntity.ok("회원가입이 완료되었습니다. 이메일을 확인해주세요.");
     }
 
-    @PostMapping("/email")
+    @PostMapping("/auth/email")
     public ResponseEntity<Map<String, Object>> checkEmailDuplicate(@RequestBody @Valid EmailCheckRequestDto requestDto) {
         boolean exists = memberService.isEmailDuplicate(requestDto.getEmail());
 
@@ -43,5 +42,19 @@ public class MemberController {
             response.put("message", "사용가능한 이메일입니다.");
             return ResponseEntity.ok(response);
         }
+    }
+
+    @GetMapping("/users/me")
+    public ResponseEntity<ApiResponseDto<MyProfileResponseDto>> getMyProfile() {
+
+        MyProfileResponseDto responseDto = memberService.getMyProfile();
+
+        ApiResponseDto<MyProfileResponseDto> response = new ApiResponseDto<>(
+                200,
+                "User information retrieved successfully.",
+                responseDto
+        );
+
+        return ResponseEntity.ok(response);
     }
 }
