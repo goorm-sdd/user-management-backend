b125ddc6c9839d208221479469036466e93915e2
projectmiluju <dnjsdyd0712@naver.com>
2025-06-15 07:57:56 +0900

    feat(update): 비밀번호 변경을 위한 UpdatePasswordRequestDto 추가 및 유효성 검사 적용

---
+++ src/main/java/org/example/goormssd/usermanagementbackend/dto/request/UpdatePasswordRequestDto.java
@@ -0,0 +1,26 @@
+package org.example.goormssd.usermanagementbackend.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.goormssd.usermanagementbackend.util.NoTripleRepeat;
+
+@Getter
+@Setter
+public class UpdatePasswordRequestDto {
+
+    @NotBlank(message = "비밀번호는 필수 입력입니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+            message = "비밀번호는 대소문자, 숫자, 특수문자를 포함해야 합니다."
+    )
+    @NoTripleRepeat
+    private String newPassword;
+
+    @NotBlank(message = "비밀번호 확인은 필수 입력입니다.")
+    private String newPasswordCheck;
+
+}
