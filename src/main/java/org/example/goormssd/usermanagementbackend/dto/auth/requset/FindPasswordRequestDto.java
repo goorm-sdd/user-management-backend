71bff4cfcb32061c1194a501146b5b5a3045b5d5
projectmiluju <dnjsdyd0712@naver.com>
2025-06-15 23:19:49 +0900

    feat: FindPasswordRequestDto에 이름, 이메일, 인증번호 유효성 검증 추가

--- src/main/java/org/example/goormssd/usermanagementbackend/dto/auth/requset/FindPasswordRequestDto.java
+++ src/main/java/org/example/goormssd/usermanagementbackend/dto/auth/requset/FindPasswordRequestDto.java
@@ -1,16 +1,25 @@
 package org.example.goormssd.usermanagementbackend.dto.auth.requset;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class FindPasswordRequestDto {
+
+    @NotBlank(message = "이름은 필수 입력입니다.")
     private String username;
+
+    @NotBlank(message = "이메일은 필수 입력입니다.")
+    @Email(message = "유효한 이메일 형식을 입력해주세요.")
     private String email;
 
     @NotBlank(message = "전화번호는 필수 입력입니다.")
     @Pattern(regexp = "^010\\d{8}$", message = "올바른 형식의 전화번호여야 합니다.")
     private String phoneNumber;
+
+    @NotBlank(message = "인증번호는 필수 입력입니다.")
+    @Pattern(regexp = "^\\d{6}$", message = "인증번호는 숫자 6자리여야 합니다.")
     private String code;
 }
