625c1483d89e46fadb2fc2ce2b9ba2610a328fb2
projectmiluju <dnjsdyd0712@naver.com>
2025-06-15 23:17:50 +0900

    refactor: LoginRequestDto에서 과도한 비밀번호 유효성 검증 제거

--- src/main/java/org/example/goormssd/usermanagementbackend/dto/auth/requset/LoginRequestDto.java
+++ src/main/java/org/example/goormssd/usermanagementbackend/dto/auth/requset/LoginRequestDto.java
@@ -22,12 +22,6 @@ public class LoginRequestDto {
     private String email;
 
     @NotBlank(message = "비밀번호는 필수 입력입니다.")
-    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
-    @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
-            message = "비밀번호는 대소문자, 숫자, 특수문자를 포함해야 합니다."
-    )
-    @NoTripleRepeat
     private String password;
 
 
