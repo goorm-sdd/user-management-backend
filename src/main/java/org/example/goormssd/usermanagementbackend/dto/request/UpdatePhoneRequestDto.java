94604a35a3b9fb4b37db981225e167a40ab35fd1
projectmiluju <dnjsdyd0712@naver.com>
2025-06-15 09:16:12 +0900

    feat(dto): 전화번호 업데이트 요청 DTO 추가 및 유효성 검사 구현

---
+++ src/main/java/org/example/goormssd/usermanagementbackend/dto/request/UpdatePhoneRequestDto.java
@@ -0,0 +1,13 @@
+package org.example.goormssd.usermanagementbackend.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+@Getter
+public class UpdatePhoneRequestDto {
+
+    @NotBlank(message = "전화번호는 필수 입력입니다.")
+    @Pattern(regexp = "^010\\d{8}$", message = "올바른 형식의 전화번호여야 합니다.")
+    private String phoneNumber;
+}
